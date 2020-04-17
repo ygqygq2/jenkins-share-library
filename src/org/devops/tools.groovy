40f1e2dd6cdc71526bae2850fb9daaa311e97f9e
ygqygq2 <29ygq@sina.com>
2020-04-17 09:06:06 +0800

    fix: 添加颜色输出

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,10 +1,10 @@
 package org.devops
 
 def colorPrint(color, context) {
-  colors = ['red'  : "\033[1;31m ${value} \033[0m",
-            'blue' : "\033[1;34m ${value} \033[0m",
-            'yello': "\033[1;33m ${value} \033[0m",
-            'green': "\033[1;32m ${value} \033[0m"
+  colors = ['red'  : "\033[1;31m ${context} \033[0m",
+            'blue' : "\033[1;34m ${context} \033[0m",
+            'yello': "\033[1;33m ${context} \033[0m",
+            'green': "\033[1;32m ${context} \033[0m"
   ]
   ansiColor('xterm') {
     println(colors[color])

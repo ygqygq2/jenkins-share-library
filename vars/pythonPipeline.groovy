3ef1376c2bed4bc59982968e9a98482e88c9ddb2
ygqygq2 <29ygq@sina.com>
2021-01-22 09:40:36 +0800

    test: test

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -1,26 +1,20 @@
 def call(String type,Map map) {
-
-    if (type == "test") {
-        pipeline {
-
-            agent {
-                node {
-                    label "${map.node}"
-                }
-            }
-
-
-            //步骤设置
-            stages {
-
-                stage('测试') {
-                    steps {
-                        println "test"
-                        }
-                    }
-                }
-
-            }
+  if (type == "test") {
+    pipeline {
+      agent {
+        node {
+          label "${map.node}"
         }
+      }
+
+      //步骤设置
+      stages {
+        stage('测试') {
+          steps {
+            println "test"
+          }
+        }
+      }
     }
+  }
 }
